00922308e0552e08c0ec120241f44dd85af40f1e
Amos Vryhof <amos@vryhofresearch.com>
2020-11-09 11:48:00 -0500

    Tes5t if a form is created

--- djangocms_forms/tests.py
+++ djangocms_forms/tests.py
@@ -1,12 +1,16 @@
 from django.test import TestCase
 
-from djangocms_forms.models import FormDefinition
+from djangocms_forms.models import FormDefinition, Form
 
 
 class ModelTestCase(TestCase):
     def setUp(self):
         FormDefinition.objects.create(
-            name="Test Form",
-            title="Test Form",
-            description="Test Form",
+            name="Test Form", title="Test Form", description="Test Form",
         )
+
+    def test_form_exists(self):
+        try:
+            test_form = Form.objects.get(name="Test Form")
+        except Form.DoesNotExist:
+            pass
